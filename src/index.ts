e4a1f882931f80cf769b530d2bbbde9f2bd2c456
Alexvy <alexvasuni@gmail.com>
2025-12-16 20:52:14 -0600

    Initialize TypeScript Express application.

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, {Express, Request, Response} from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, TypeScript with Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
